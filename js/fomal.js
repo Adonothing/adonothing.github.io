582b8a80885cf7afa1e6cfaa094371532a96441b
Adonothing <2313047741@qq.com>
2023-12-02 20:37:46 +0800

    Site updated: 2023-12-02 20:37:43

--- js/fomal.js
+++ js/fomal.js
@@ -3174,10 +3174,10 @@ if (localStorage.getItem("blogbg") != undefined) {
   setBg(localStorage.getItem("blogbg"));
 } else {
   document.getElementById("defineBg").innerText = `:root{
-    --default-bg: url(https://lskypro.acozycotage.net/Fomalhaut/img/dm14.webp);
-    --darkmode-bg:url(https://lskypro.acozycotage.net/Fomalhaut/img/yuanshen1.webp);
-    --mobileday-bg: url(https://lskypro.acozycotage.net/Fomalhaut/img/snow.webp);
-    --mobilenight-bg: url(https://lskypro.acozycotage.net/Fomalhaut/img/mb8.webp);
+    --default-bg: url(https://picture.adunas.top/yys/SP-YuanJieShenG-2732x2048.jpg);
+    --darkmode-bg:url(https://picture.adunas.top/yys/SP-YuanJieShenG-2732x2048.jpg);
+    --mobileday-bg: url(https://picture.adunas.top/yys/SP-YuanJieShenG-2732x2048.jpg);
+    --mobilenight-bg: url(https://picture.adunas.top/yys/SP-YuanJieShenG-2732x2048.jpg);
   }`;
 }
 // 切换背景主函数
