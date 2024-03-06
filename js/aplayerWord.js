e1d9e544d42dd711120d5504e982259d82d0cbd0
Adonothing <2313047741@qq.com>
2024-03-07 02:23:20 +0800

    Site updated: 2024-03-07 02:23:03

--- js/aplayerWord.js
+++ js/aplayerWord.js
@@ -213,6 +213,15 @@ const ap3 = new APlayer({
         },
         // 0007
         {
+            name: 'nevertheless',
+            artist: 'Adunas',
+            url: 'https://picture.adunas.top/Audio/English/neverthelessAdunasA.mp3',
+            cover: 'cover2.jpg',
+            //lrc: 'lrc1.lrc',
+            theme: '#46718b'
+        },
+        // 0007
+        {
             name: 'object',
             artist: 'Adunas',
             url: 'https://picture.adunas.top/Audio/English/objectAdunasA.mp3',
