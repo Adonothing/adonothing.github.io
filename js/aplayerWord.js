29c4726abdbb8cdcb327bc553aa654434f3971f6
Adonothing <2313047741@qq.com>
2024-03-07 01:34:01 +0800

    Site updated: 2024-03-07 01:33:45

--- js/aplayerWord.js
+++ js/aplayerWord.js
@@ -321,6 +321,15 @@ const ap3 = new APlayer({
         },
         // 0011
         {
+            name: 'stationary',
+            artist: 'Adunas',
+            url: 'https://picture.adunas.top/Audio/English/stationaryAdunasA.mp3',
+            cover: 'cover2.jpg',
+            //lrc: 'lrc1.lrc',
+            theme: '#46718b'
+        },
+        // 0011
+        {
             name: 'subject',
             artist: 'Adunas',
             url: 'https://picture.adunas.top/Audio/English/subjectAdunasA.mp3',
